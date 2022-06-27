ae62d2febda7a0404b3b50672382dffdcdd95597
Julie Nisbet <jmnisbet@gmail.com>
2022-06-27 08:35:47 -0700

    removing supabase

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,11 +1,4 @@
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
-
-const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
-
-export function getUser() {
-    return client.auth.session() && client.auth.session().user;
-}
+export function getUser() {}
 
 export function checkAuth() {
     const user = getUser();
@@ -19,24 +12,8 @@ export function redirectIfLoggedIn() {
     }
 }
 
-export async function signupUser(email, password) {
-    const response = await client.auth.signUp({ email, password });
+export async function signupUser(email, password) {}
 
-    return response.user;
-}
+export async function signInUser(email, password) {}
 
-export async function signInUser(email, password) {
-    const response = await client.auth.signIn({ email, password });
-
-    return response.user;
-}
-
-export async function logout() {
-    await client.auth.signOut();
-
-    return (window.location.href = '../');
-}
-
-// function checkError({ data, error }) {
-//     return error ? console.error(error) : data;
-// }
+export async function logout() {}
